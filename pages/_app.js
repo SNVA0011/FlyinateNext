f349ba645ebc799dd03fee69bc495d5778928ff5
diptanshu <96859907+SNVA0011@users.noreply.github.com>
2022-05-26 11:29:30 -0500

    css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
+import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return(
