1a40bd0926cfba409c7e8fbd76c1118d5f7345f3
diptanshu <96859907+SNVA0011@users.noreply.github.com>
2022-06-01 17:02:44 -0500

    Adding Google tag manager & Google  Analytics, as given by mahaveer sir

--- pages/_document.js
+++ pages/_document.js
@@ -15,18 +15,20 @@ class MyDocument extends Document {
         <Head>
 
           {/* <!-- Google Tag Manager --> */}
-          <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TP4XD9K');`}</script>
+})(window,document,'script','dataLayer','GTM-TP4XD9K');`}}></script>
           {/* <!-- End Google Tag Manager --> */}
         </Head>
         <body>
 
           {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TP4XD9K"
-            height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TP4XD9K"
+            height="0" width="0"  style="display:none;visibility:hidden"></iframe>`}}></noscript>
           {/* <!-- End Google Tag Manager (noscript) --> */}
 
           <Main />
