27cdbe3e960cf7d5e5a48da73b5f47a0dd46e97d
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2022-12-13 10:32:02 +0530

    sitemap update

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -69,37 +69,43 @@ export const getServerSideProps = async (ctx) => {
 
   //========== English (en) ==========
   const staticUrl = [
-    {'url':'','time':'2022-04-30T04:40:48.000Z'},
-    {'url':"about-us",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"flights",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"contact",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"privacy",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"terms",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"cheap-flights/machu-picchu-flyinate",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"cheap-flights/galapagos-islands-flyinate",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"cheap-flights/cusco-flyinate",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"cheap-flights/easter-island-flyinate",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"cheap-flights/buenos-aires-flyinat",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"cheap-flights/lima-flyinate",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"customize/business-travel",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"customize/group-travel",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':'blog','time':new Date().toISOString()}
+    {'url':'','time':'2022-08-30T04:40:48','priority':'1.00'},
+    {'url':"about-us",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"flights",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"contact",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"privacy",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"terms",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"cheap-flights/machu-picchu-flyinate",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"cheap-flights/galapagos-islands-flyinate",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"cheap-flights/cusco-flyinate",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"cheap-flights/easter-island-flyinate",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"cheap-flights/buenos-aires-flyinat",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"cheap-flights/lima-flyinate",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"customize/business-travel",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"customize/group-travel",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':'blog','time': new Date().toISOString().slice(0, -5),'priority':'0.85'}
   ]
   // staticUrl
   const staticSitemaps = staticUrl.map((item) => ({
     loc: `${baseUrl}/${item.url.replace('&', '&amp;')}`,
-    lastmod: new Date(item.time).toISOString(),
+    lastmod: new Date(item.time).toISOString().slice(0, -5),
+    changefreq: 'daily',
+    priority : item.priority
   }));
   // blog
   const blogSitemaps = posts && posts.map((item) => ({
     loc: `${baseUrl}/blog/${item.titleUrl.replace('&', '&amp;')}`,
-    lastmod: new Date(item.posttime).toISOString(),
+    lastmod: new Date(item.posttime).toISOString().slice(0, -5),
+    changefreq: 'daily',
+    priority : '0.85'
   }));
 
   // flights
   const flightsSitemaps = flightsposts && flightsposts.filter((items) => items.pageType === "Airline").map((items) => ({
     loc: `${baseUrl}/flights/${items.url.replace('&', '&amp;')}-${items.pageValue.replace('&', '&amp;')}`,
-    lastmod: '2022-04-30T04:40:48.000Z',
+    lastmod: '2022-08-30T04:40:48',
+    changefreq: 'daily',
+    priority : '0.69'
   }));
 
 
@@ -151,28 +157,34 @@ export const getServerSideProps = async (ctx) => {
 
   //========== Spanish (es) ==========
   const esStaticUrl = [
-    {'url':'','time':'2022-04-30T04:40:48.000Z'},
-    {'url':"sobre-nosotras",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"vuelos",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"articulos",'time':new Date().toISOString()},
-    {'url':"contacto",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"politica",'time':'2022-04-30T04:40:48.000Z'},
-    {'url':"terminos",'time':'2022-04-30T04:40:48.000Z'}
+    {'url':'','time':'2022-08-30T04:40:48','priority':'0.85'},
+    {'url':"sobre-nosotras",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"vuelos",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"articulos",'time':new Date().toISOString().slice(0, -5),'priority':'0.69'},
+    {'url':"contacto",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"politica",'time':'2022-08-30T04:40:48','priority':'0.69'},
+    {'url':"terminos",'time':'2022-08-30T04:40:48','priority':'0.69'}
   ]
   // esStaticUrl
   const esStaticSitemaps = esStaticUrl.map((item) => ({
     loc: `${baseUrl}/es/${item.url.replace('&', '&amp;')}`,
-    lastmod: new Date(item.time).toISOString(),
+    lastmod: new Date(item.time).toISOString().slice(0, -5),
+    changefreq: 'daily',
+    priority : item.priority
   }));
   // blog
   const esBlogSitemaps = esposts && esposts.map((item) => ({
     loc: `${baseUrl}/es/articulos/${item.titleUrl.replace('&', '&amp;')}`,
-    lastmod: new Date(item.posttime).toISOString(),
+    lastmod: new Date(item.posttime).toISOString().slice(0, -5),
+    changefreq: 'daily',
+    priority : '0.56'
   }));
   // flights
   const esFlightsSitemaps = esFlightsposts && esFlightsposts.filter((item) => item.pageType === "AirlineE").map((item) => ({
     loc: `${baseUrl}/es/vuelos/${item.url.replace('&', '&amp;')}-${item.pageValue.replace('&', '&amp;')}`,
-    lastmod: '2022-04-30T04:40:48.000Z',
+    lastmod: '2022-08-30T04:40:48',
+    changefreq: 'daily',
+    priority : '0.69'
   }));
 
 
