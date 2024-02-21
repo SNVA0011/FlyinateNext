d3a2b982383dbbc2b275fc3e76c2f8d2bac35b82
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2024-02-21 15:45:07 +0530

    git push

--- component/PageHead.js
+++ component/PageHead.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import React from 'react'
 import { useRouter } from 'next/router' 
-import { siteurl } from '../utils/static'
+import { baseUrl } from '../utils/static'
 
 
 const PageHead = ({ title, description, keywords }) => {
@@ -12,8 +12,8 @@ const PageHead = ({ title, description, keywords }) => {
                 <title>{title}</title>
                 <meta name="description" content={description} />
                 <meta name="keywords" content={keywords} />
-                <link rel="canonical" href={siteurl + router.asPath} />
-                <link rel="alternate" href={siteurl + router.asPath} />
+                <link rel="canonical" href={baseUrl + router.asPath} />
+                <link rel="alternate" href={baseUrl + router.asPath} />
             </Head>
         </>
     )
